fe61ba8ccf973c81bb49e43dcc9a2e286d4aa9a0
Thomas Gossard <thomas.gossard@uni-tuebingen.de>
2025-06-03 17:03:45 +0200

    Refactored hough_bundler.py

--- tt3d/calibration/hough_bundler.py
+++ tt3d/calibration/hough_bundler.py
@@ -14,35 +14,9 @@ class HoughBundler:
         self.min_angle = min_angle
 
     def get_orientation(self, line):
-        orientation = math.atan2((line[3] - line[1]), (line[2] - line[0]))
-        return math.degrees(orientation)
-
-    def check_is_line_different(
-        self, line_1, groups, min_distance_to_merge, min_angle_to_merge
-    ):
-        for group in groups:
-            for line_2 in group:
-                # print()
-                # print(line_1)
-                # print(line_2)
-                # print(self.get_distance(line_2, line_1))
-                # print()
-                if self.get_distance(line_2, line_1) < min_distance_to_merge:
-                    orientation_1 = self.get_orientation(line_1)
-                    orientation_2 = self.get_orientation(line_2)
-                    # print(orientation_1)
-                    # print(orientation_2)
-                    # print(orientation_1 - orientation_2)
-                    delta_orientation = abs(orientation_1 - orientation_2)
-                    if delta_orientation < -90:
-                        delta_orientation += 180
-                    elif delta_orientation > 90:
-                        delta_orientation -= 180
-                    # print(delta_orientation)
-                    if delta_orientation < min_angle_to_merge:
-                        group.append(line_1)
-                        return False
-        return True
+        x1, y1, x2, y2 = line
+        angle_rad = math.atan2(y2 - y1, x2 - x1)
+        return math.degrees(angle_rad)
 
     def point_to_segment_distance(self, point, line):
         px, py = point
@@ -52,76 +26,80 @@ class HoughBundler:
             return np.hypot(px - x1, py - y1)
         u = ((px - x1) * (x2 - x1) + (py - y1) * (y2 - y1)) / (line_mag**2)
         if u < 0:
-            closest_x, closest_y = x1, y1
-        elif u > 0:
-            closest_x, closest_y = x2, y2
+            closest = (x1, y1)
+        elif u > 1:
+            closest = (x2, y2)
         else:
-            closest_x = x1 + u * (x2 - x1)
-            closest_y = y1 + u * (y2 - y1)
-        return np.hypot(px - closest_x, py - closest_y)
+            closest = (x1 + u * (x2 - x1), y1 + u * (y2 - y1))
+        return np.hypot(px - closest[0], py - closest[1])
 
-    def get_distance(self, a_line, b_line):
-        distances = [
-            self.point_to_segment_distance(a_line[:2], b_line),
-            self.point_to_segment_distance(a_line[2:], b_line),
-            self.point_to_segment_distance(b_line[:2], a_line),
-            self.point_to_segment_distance(b_line[:2], a_line),
-        ]
+    @staticmethod
+    def ccw(A, B, C):
+        return (C[1] - A[1]) * (B[0] - A[0]) > (B[1] - A[1]) * (C[0] - A[0])
 
-        return min(distances)
-        # print(np.min(np.array(distances)))
-        # return np.min(np.array(distances))
+    def do_lines_intersect(self, line1, line2):
+        A = line1[:2]
+        B = line1[2:]
+        C = line2[:2]
+        D = line2[2:]
+        return (self.ccw(A, C, D) != self.ccw(B, C, D)) and (
+            self.ccw(A, B, C) != self.ccw(A, B, D)
+        )
+
+    def get_distance(self, line1, line2):
+        if self.do_lines_intersect(line1, line2):
+            return 0.0
+        return min(
+            [
+                self.point_to_segment_distance(line1[:2], line2),
+                self.point_to_segment_distance(line1[2:], line2),
+                self.point_to_segment_distance(line2[:2], line1),
+                self.point_to_segment_distance(line2[2:], line1),
+            ]
+        )
+
+    def check_is_line_different(self, line, groups, min_dist, min_angle):
+        for group in groups:
+            for ref_line in group:
+                if self.get_distance(line, ref_line) < min_dist:
+                    angle1 = self.get_orientation(line)
+                    angle2 = self.get_orientation(ref_line)
+                    delta_angle = abs(angle1 - angle2)
+                    delta_angle = min(delta_angle, 180 - delta_angle)  # normalize
+                    if delta_angle < min_angle:
+                        group.append(line)
+                        return False
+        return True
 
     def merge_lines_into_groups(self, lines):
-        groups = []  # all lines groups are here
-        # first line will create new group every time
-        groups.append([lines[0]])
-        # print(lines[0])
-        # if line is different from existing gropus, create a new group
-        for line_new in lines[1:]:
+        if len(lines) == 0:
+            return []
+        groups = [[lines[0]]]
+        for line in lines[1:]:
             if self.check_is_line_different(
-                line_new, groups, self.min_distance, self.min_angle
+                line, groups, self.min_distance, self.min_angle
             ):
-                groups.append([line_new])
-
+                groups.append([line])
         return groups
 
     def merge_line_segments(self, lines):
-        orientation = self.get_orientation(lines[0])
+        # Flip line direction if needed for consistency
+        base_angle = self.get_orientation(lines[0])
         for i, line in enumerate(lines):
-            if abs(self.get_orientation(line) - orientation) > 90:
-                lines[i] = np.concatenate((line[2:], line[:2]))
-        x1_mean = np.mean([line[0] for line in lines])
-        y1_mean = np.mean([line[1] for line in lines])
-        x2_mean = np.mean([line[2] for line in lines])
-        y2_mean = np.mean([line[3] for line in lines])
-        return [x1_mean, y1_mean, x2_mean, y2_mean]
-
-    # def merge_line_segments(self, lines):
-    #     orientation = self.get_orientation(lines[0])
-
-    #     if len(lines) == 1:
-    #         return np.block([[lines[0][:2], lines[0][2:]]])
-
-    #     points = []
-    #     for line in lines:
-    #         points.append(line[:2])
-    #         points.append(line[2:])
-    #     if 45 < orientation <= 90:
-    #         # sort by y
-    #         points = sorted(points, key=lambda point: point[1])
-    #     else:
-    #         # sort by x
-    #         points = sorted(points, key=lambda point: point[0])
-
-    #     return np.block([[points[0], points[-1]]])
+            angle = self.get_orientation(line)
+            if abs(angle - base_angle) > 90:
+                lines[i] = [line[2], line[3], line[0], line[1]]
+        # Average endpoints
+        x1 = np.mean([line[0] for line in lines])
+        y1 = np.mean([line[1] for line in lines])
+        x2 = np.mean([line[2] for line in lines])
+        y2 = np.mean([line[3] for line in lines])
+        return [x1, y1, x2, y2]
 
     def process_lines(self, lines):
+        if len(lines) == 0:
+            return np.empty((0, 4), dtype=float)
+        lines = np.asarray(lines)
         groups = self.merge_lines_into_groups(lines)
-        merged_lines = []
-        for group in groups:
-            merged_lines.append(self.merge_line_segments(group))
-        merged_lines = np.asarray(merged_lines).squeeze()
-        if len(merged_lines.shape) == 1:
-            return merged_lines[None, :]
-        return merged_lines
+        merged_lines = [self.merge_line_segments(group) for group in groups]
+        return np.array(merged_lines)
